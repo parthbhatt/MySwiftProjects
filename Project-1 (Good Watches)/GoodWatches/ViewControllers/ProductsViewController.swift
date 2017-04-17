ab136ba61a0e3a84bea3721305e647d2b70e9ca8
Parth Bhatt <parth19888@gmail.com>
2017-04-17 18:45:22 +0530

    Added animation code and comments
    - Added code for animation for image view in Tableviewcell.
    - Added comments to code.
    - Removed some unused code.

--- Project-1 (Good Watches)/GoodWatches/ViewControllers/ProductsViewController.swift
+++ Project-1 (Good Watches)/GoodWatches/ViewControllers/ProductsViewController.swift
@@ -11,35 +11,57 @@ import UIKit
 let themeColor = UIColor(red: 0.5, green: 0.0, blue: 0.0, alpha: 1.0)
 
 
-class ProductsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+class ProductsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
+    
     @IBOutlet weak var tblProducts: UITableView!
     
     fileprivate let productIdentifier = "productTableViewCell"
     
     var arrProducts: [Product<NSObject>]?
     
+    var arrIndex:NSMutableArray = []
     
+    //MARK: - View Life cycle methods
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-         arrProducts = [Product(name: "Hublot", imageName: "hublot-logo", fullImageName: "hublot_watch"),Product(name: "Rolex", imageName: "rolex-logo", fullImageName: "rolex_watch"),Product(name: "Omega", imageName: "omega-logo", fullImageName: "omega_watch")];
         
+        // Array of Products to be displayed
+        arrProducts = [Product(name: "Hublot", imageName: "hublot-logo", fullImageName: "hublot_watch"),
+                       Product(name: "Rolex", imageName: "rolex-logo", fullImageName: "rolex_watch"),
+                       Product(name: "Omega", imageName: "omega-logo", fullImageName: "omega_watch")];
+        
+        
+        // Added index array to avoid animation of imageview every time while scrolling.
+        if let prodArray = arrProducts {
+            for item in prodArray {
+                print(item)
+                arrIndex.add(0)
+            }
+        }
+        
+        //Added title to top bar along with the bar tint color and title text attributes
         self.navigationItem.title = "Watches"
         self.navigationController?.navigationBar.barTintColor = themeColor;
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         
+        //Added bar tint color and title & image tint color to bottom bar
         self.tabBarController?.tabBar.barTintColor = UIColor.white
         self.tabBarController?.tabBar.tintColor = themeColor
         
+        //Reload table view.
         tblProducts.dataSource = self
         tblProducts.delegate = self
         tblProducts.reloadData()
     }
     
-    //MARK: Table View Datasource and Delegates
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Table View Datasource and Delegates
     func numberOfSections(in tableView: UITableView) -> Int
     {
         return 1;
@@ -65,37 +87,38 @@ class ProductsViewController: UIViewController,UITableViewDelegate,UITableViewDa
         cell.separatorInset = UIEdgeInsets.zero
         cell.layoutMargins = UIEdgeInsets.zero
         
+        //Assigning product information to cell.
         cell.textLabel?.text = product?.name
         if let imageName = product?.imageName {
             cell.imageView?.image = UIImage(named: imageName)
         }
         
+        animateImageIfNeeded(forCell: cell, atIndex: indexPath)
+        
         cell.selectionStyle = UITableViewCellSelectionStyle.none
         cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
         
         return cell
-        
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
         return 60;
     }
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //MARK: - Custom Metods
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //This method is used to animate the image the first time a cell is created at a particular indexPath
+    func animateImageIfNeeded(forCell cell: UITableViewCell, atIndex indexPath: IndexPath)
+    {
+        //Checks if cell is created for the first time at a given indexPath
+        if((arrIndex[indexPath.row] as? Int) == 0)
+        {
+            cell.imageView?.transform = CGAffineTransform(scaleX: 0, y: 0);
+            UIView.animate(withDuration: 2, animations: {
+                cell.imageView?.transform = CGAffineTransform.identity
+            })
+            arrIndex.replaceObject(at:indexPath.row, with: 1);
+        }
+    }    
 }
